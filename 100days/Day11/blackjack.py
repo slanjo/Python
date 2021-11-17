da14d8434b28168a5f56fd070d17df62a3d4b636
slanjo <slaanjoo@gmail.com>
2021-11-17 13:49:56 +0000

    Day-11

--- 100days/Day11/blackjack.py
+++ 100days/Day11/blackjack.py
@@ -1,16 +1,23 @@
+import random
 from os import system
 from art import logo
 print(logo)
 
-def deal():
+def deal(player_cards, cards):
+
     print("Dealing first two cards\n")
+    num_of_cards = len(cards)
+    player_cards.append(random.choice(cards))
+    return sum(player_cards)
 
 def deal_another():
     print("Dealing Another card\n")
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-player = []
-dealer = []
+player_cards = []
+dealer_cards = []
+player_score = 0
+dealer_score = 0
 play = True
 
 while play:
@@ -23,11 +30,14 @@ while play:
 
     else:
         another_card = True
+        deal(player_cards, cards)
+        print(player_cards)
+        print(deal(player_cards, cards))
+        
         while another_card:
             one_more_card = input("Type 'y' to get another card, type 'n' to pass: ").lower()
 
-
-
+            deal(player_cards, cards )
 
             if one_more_card == 'n':
                 another_card = False
