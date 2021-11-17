c138e350e5a2141c37c330f4c469d7b1efe40dff
slanjo <slaanjoo@gmail.com>
2021-11-17 23:53:58 +0000

    Day-11

--- 100days/Day11/blackjack.py
+++ 100days/Day11/blackjack.py
@@ -3,31 +3,28 @@ from os import system
 from art import logo
 print(logo)
 
-def deal(player_cards, cards, more):
+def deal(current_card_list, cards, more):
 
-    print("Dealing first two cards\n")
     num_of_cards = len(cards)
-    if more:
-        player_cards.append(random.choice(cards))
-        player_cards.append(random.choice(cards))
+    card = 0
+    card = random.choice(cards)
+    if card == 11 and sum(current_card_list) + 11 > 21:
+        return 1 
     else:
-        player_cards.append(random.choice(cards))
+        return card 
 
-    print(player_cards)
-    return sum(player_cards)
+def totals(current_card_list):
 
-
-def deal_another():
-    print("Dealing Another card\n")
+    return sum(current_card_list)
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 player_cards = []
 dealer_cards = []
 player_score = 0
 dealer_score = 0
-more = False
+more = True 
 play = True
-total = 0
+
 while play:
 
 #### Terminate game
@@ -38,18 +35,66 @@ while play:
 
     else:
         another_card = True
-        print(deal(player_cards, cards, more))
-        print(deal(dealer_cards, cards, more)) 
+        system('clear')
+        print(logo)
+        player_cards = []
+        dealer_cards = []
+        player_score = 0
+        dealer_score = 0
+
+#Player first two cards
+        player_cards.append(deal(player_cards, cards, more))
+        player_cards.append(deal(player_cards, cards, more))
+        print(f"Your cards: {player_cards}, current score {sum(player_cards)}")
+
+#Dealer first two cards
+        dealer_cards.append(deal(dealer_cards, cards, more))
+        dealer_cards.append(deal(dealer_cards, cards, more))
+        print(f"Computer's first card: {dealer_cards[0]}")
 
         while another_card:
+
             one_more_card = input("Type 'y' to get another card, type 'n' to pass: ").lower()
 
             if one_more_card == 'n':
+                player_score = sum(player_cards) 
                 another_card = False
-            else:
-                total += deal(player_cards,cards, more)
 
+            else:
+                player_cards.append(deal(player_cards, cards, more))
+                player_score = sum(player_cards) 
+                print(f"Your cards {player_cards}, current score: {player_score}")
+
+            if dealer_score < 17:
+                dealer_cards.append(deal(dealer_cards, cards, more)) 
+                dealer_score = sum(dealer_cards) 
+
+        if player_score == dealer_score:
+            print(f"    Your final hand: {player_cards}, final score: {player_score}")
+            print(f"    Computer's final hand: {dealer_score}, final score {dealer_score}")
+            print("Draw")   
+
+        elif player_score > 21:
+
+            print(f"    Your final hand: {player_cards}, final score: {player_score}")
+            print(f"    Computer's final hand: {dealer_score}, final score {dealer_score}")
+            print("You went over. You lose")   
+
+        elif dealer_score > 21: 
+            print(f"    Your final hand: {player_cards}, final score: {player_score}")
+            print(f"    Computer's final hand: {dealer_score}, final score {dealer_score}")
+            print("You win!")   
+
+        elif player_score > dealer_score:
+
+            print(f"    Your final hand: {player_cards}, final score: {player_score}")
+            print(f"    Computer's final hand: {dealer_score}, final score {dealer_score}")
+            print("You win!")   
         
 
+        elif player_score < dealer_score:
 
-
+            print(f"    Your final hand: {player_cards}, final score: {player_score}")
+            print(f"    Computer's final hand: {dealer_score}, final score {dealer_score}")
+            print("You lose")   
+            
