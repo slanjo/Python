85b395ac0146a5c91fab2a5fedb5c1b7770135dd
“slanjo” <slanjo@protonmail.com>
2021-11-24 06:48:34 +0800

    Day15

--- 100days/Day15/main.py
+++ 100days/Day15/main.py
@@ -1,3 +1,4 @@
+from os import system
 MENU = {
     "espresso": {
         "ingredients": {
@@ -26,7 +27,51 @@ MENU = {
 
 resources = {
     "water": 300,
-    "milk": 200,
+#    "water": 40,
+#    "milk": 200,
     "coffee": 100,
+    "milk": 0,
 }
 
+
+def drink():
+    report()
+
+
+def report_not_enough_resource(ingredient):
+    print(f"Sorry there is not enough {ingredient}")
+
+
+def report():
+    print(resources)
+
+
+def check_resources(choice):
+    for resource in MENU[choice]["ingredients"]:
+        if MENU[choice]["ingredients"][resource] > resources[resource]:
+            print(f'{report_not_enough_resource(MENU[choice]["ingredients"][resource])}')
+            print(f'{report_not_enough_resource(resource)}')
+
+def process_request(choice):
+    if choice == "latte":
+        price = 2.5
+        check_resources("latte")
+    elif choice == "cappuccino":
+        price = 3.0
+        print("cappuccino")
+    elif choice == "espresso":
+        price = 2.0
+        print("espresso")
+
+
+machine_on = True
+
+
+while machine_on:
+    selection = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if selection == "off":
+        machine_on = False
+    elif selection == "report":
+        report()
+    elif selection == "espresso" or selection == "latte" or selection == "cappuccino":
+        process_request(selection)
