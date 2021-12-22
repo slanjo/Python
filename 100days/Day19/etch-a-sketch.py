1291ffca2194967eeb59cdc3bc284fbe1c728319
slanjo <slaanjoo@gmail.com>
2021-12-22 22:50:43 +0800

    Day 19

---
+++ 100days/Day19/etch-a-sketch.py
@@ -0,0 +1,26 @@
+from turtle import Turtle, Screen
+
+def mv_fd():
+    korni.fd(10)
+def mv_bck():
+    korni.back(10)
+def mv_clck():
+    korni.setheading(korni.heading() + 10) 
+def mv_cnt_clck():
+    korni.setheading(korni.heading() - 10) 
+def clr_scrn():
+    my_screen.clearscreen()
+
+if __name__ == '__main__':
+    korni = Turtle()
+    my_screen = Screen()
+    my_screen.listen() 
+
+    my_screen.onkey(fun=mv_fd, key="w")
+    my_screen.onkey(fun=mv_fd, key="w")
+    my_screen.onkey(fun=mv_bck, key="s")
+    my_screen.onkey(fun=mv_cnt_clck, key="a")
+    my_screen.onkey(fun=mv_clck, key="d")
+    my_screen.onkey(fun=clr_scrn, key="c")
+
+    my_screen.exitonclick()
