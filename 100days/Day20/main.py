04addc609e61376fdb6130f3d4e41cf4be5b9a9c
slanjo <slaanjoo@gmail.com>
2022-01-20 07:05:00 +0800

    Day20

--- 100days/Day20/main.py
+++ 100days/Day20/main.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
 
 
@@ -9,27 +10,18 @@ if __name__ == '__main__':
     screen.title("Snake Game")
     screen.tracer(0)
 
-    starting_position = [(0, 0), (-20, 0), (-40, 0)]
-    segments = []
-    for position in starting_position:
-        new_segment = Turtle("square")
-        new_segment.penup()
-        new_segment.goto(position)
-        new_segment.color("white")
-        segments.append(new_segment)
+    snake = Snake()
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
 
     game_is_on = True
     while game_is_on:
         screen.update()
         time.sleep(0.1)
-
-        for seg_num in range(len(segments) -1, 0, -1):
-            new_x = segments[seg_num - 1].xcor() 
-            new_y = segments[seg_num -1 ].ycor()
-            segments[seg_num].goto(new_x, new_y)
-
-        segments[0].forward(20)   
-        segments[0].left(90)
+        snake.move()
 #   korni_1 = Turtle(shape="square") 
 #   korni_1.color("white", "white") 
 #   korni_2 = Turtle(shape="square") 
--- 100days/Day20/snake.py
+++ 100days/Day20/snake.py
@@ -1,2 +1,37 @@
-class Snake():
-    pass
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+FORWARD_DISTANCE = 20
+from turtle import Turtle
+
+
+class Snake:
+ #        self.snake = snake
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.penup()
+            new_segment.goto(position)
+            new_segment.color("white")
+            self.segments.append(new_segment)
+
+    def move(self):
+
+        for seg_num in range(len(self.segments) -1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor() 
+            new_y = self.segments[seg_num -1 ].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+
+        self.segments[0].forward(FORWARD_DISTANCE)   
+
+    def up(self, direction):
+        self.segments[0].left(90)
+        pass
+    def down(self):
+        pass
+    def left(self):
+        pass
+    def right(self):
+        pass
